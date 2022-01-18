d83481e436a231279aa628490110d536d25ac86a
Jo-mingi <mingijo1004@naver.com>
2022-01-18 11:46:23 +0900

    REST API day02 - 02

---
+++ src/main/java/co/mr/restapi02/controller/AcademyController.java
@@ -0,0 +1,29 @@
+package co.mr.restapi02.controller;
+
+import co.mr.restapi02.mapper.AcademyMapper;
+import co.mr.restapi02.model.Academy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/academy")
+public class AcademyController {
+
+    @Autowired
+    private AcademyMapper academyMapper;
+
+    @PostMapping("")
+    // 파라미터를 requestBody로 전달하도록 설정
+    public int post(@RequestBody Academy academy){
+        return academyMapper.insert(academy);
+    }
+
+    @GetMapping("")
+    public List<Academy> getAcademies(){
+        return academyMapper.getAcademies();
+    }
+
+
+}
